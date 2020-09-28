f41552cd8da1603c6507f4a9d73b4a6e957a4062
wje <892738614@qq.com>
2020-09-29 02:24:38 +0800

    画轨迹
    画轨迹

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
   import 'echarts/extension/bmap/bmap'
   import china from './china'
   import nameMap from './nameMap'
+  import geoCoordMap from './geoCoordMap'
 
   let myCharts = null
   export default {
@@ -71,7 +72,11 @@
             left: 0,
             right: 0
           },
-        }
+        },
+        BJData: [
+          [{name:'北京'}, {name:'上海',value:95}],
+          [{name:'北京'}, {name:'广州',value:90}],
+        ]
       }
     },
     beforeDestroy () {
@@ -134,7 +139,7 @@
        * */
       getChinaOption (mapType, mapJson) {
         const self = this
-        return {
+        let  _obj =  {
           geo: {
             show: true,
             map: mapType,
@@ -172,6 +177,7 @@
             {
               name: 'Top',
               type: 'scatter',
+              zlevel: 5,
               coordinateSystem: 'geo',
               encode: {
                 value: 2
@@ -221,6 +227,7 @@
             {
               name: 'Top 5',
               type: 'effectScatter',
+              zlevel: 5,
               coordinateSystem: 'geo',
               encode: {
                 value: 2
@@ -247,9 +254,72 @@
                 name: '上海市',
                 value: self.getRandomData()
               }])
+            },
+            {
+              name: '',
+              type: 'lines',
+              zlevel: 3,
+              effect: {
+                show: true,
+                period: 6,
+                trailLength: 0.7,
+                color: '#fff',
+                symbolSize: 3
+              },
+              lineStyle: {
+                normal: {
+                  color: '#adf332',
+                  width: 0,
+                  curveness: 0.2,
+                  shadowColor:'#fff',
+                  // shadowColor: 'rgba(255, 255, 255, 0.5)',
+                  shadowBlur: 3
+                }
+              },
+              data: self._convertLineData(self.BJData)
+            },
+            {
+              name: '',
+              type: 'lines',
+              zlevel: 4,
+              symbol: ['none', 'arrow'],
+              symbolSize: 10,
+              effect: {
+                show: true,
+                period: 6,
+                trailLength: 0,
+                symbol: 'none',
+                // symbol: 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z',
+                symbolSize: 15,
+                loop: true
+              },
+              lineStyle: {
+                color: '#adf332',
+                width: 1,
+                opacity: 0.8,
+                curveness: 0.2
+              },
+              data: self._convertLineData(self.BJData)
             }
           ]
         }
+        return _obj
+      },
+      _convertLineData (data) {
+        let res = []
+        for (let i = 0; i < data.length; i++) {
+          let dataItem = data[i]
+          let fromCoord = geoCoordMap[dataItem[0].name]
+          let toCoord = geoCoordMap[dataItem[1].name]
+          if (fromCoord && toCoord) {
+            res.push({
+              fromName: dataItem[0].name,
+              toName: dataItem[1].name,
+              coords: [fromCoord, toCoord]
+            })
+          }
+        }
+        return res;
       },
       /****
        * @method 生成全省地图的配置项
